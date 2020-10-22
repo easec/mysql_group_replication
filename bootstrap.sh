6f7fbaffec74840e403084928c7ab24a1123bc04
easec <mats.johannesson@easec.se>
2020-10-22 09:21:19 +0200

    Create bootstrap.sh
    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+docker exec -it node1 mysql -uroot -peasec \
+  -e "SET @@GLOBAL.group_replication_bootstrap_group=1;" \
+  -e "create user 'repl'@'%';" \
+  -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%';" \
+  -e "flush privileges;" \
+  -e "change master to master_user='repl' for channel 'group_replication_recovery';" \
+  -e "START GROUP_REPLICATION;" \
+  -e "SET @@GLOBAL.group_replication_bootstrap_group=0;" \
+  -e "SELECT * FROM performance_schema.replication_group_members;"
