b2871af2e03f558b003de6b69d53883012abe31b
easec <mats.johannesson@easec.se>
2020-10-22 13:25:04 +0200

    Create bootstrap_node2_3.sh
    Initial commit

---
+++ bootstrap_node2_3.sh
@@ -0,0 +1,5 @@
+for N in 2 3
+do docker exec -it node$N mysql -uroot -peasec \
+  -e "change master to master_user='repl' for channel 'group_replication_recovery';" \
+  -e "START GROUP_REPLICATION;"
+done
